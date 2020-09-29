3aa2b29678bc8a1db9d9b50580fcff7b5dca4097
Ivanka Heins <shinnova@users.noreply.github.com>
2020-09-29 19:37:29 +0200

    Change url to archive instead of git tag
    Remove leftover comments

--- ffmt.rb
+++ ffmt.rb
@@ -17,11 +17,10 @@ end
 class Ffmt < Formula
   desc "FFXIV Modding Tool is the Cross-platform Commandline interface alternative for TexTools"
   homepage "https://ffmt.pwd.cat/"
-  url "https://github.com/fosspill/FFXIV_Modding_Tool.git", :tag => "v0.9.6.1", :revision => "aecef7ec6af82672fb98875500f14a7b0205d9c7"
-  #sha256 "d117dc9996a5c3dd2dc9efaa1c61032e12b72dc5e1bfa16f9ee714697e24d385"
+  url "https://github.com/fosspill/FFXIV_Modding_Tool/archive/v0.9.6.1.tar.gz"
+  sha256 "516833abe6f8e9566264641ee0aab6ef6d13cd79a46aeca5202c1a829b56a635"
   license "GPL-3.0"
 
-  # depends_on "cmake" => :build
   depends_on Dotnet => :build
 
   def install
@@ -33,15 +32,6 @@ class Ffmt < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test FFXIV_Modding_Tool`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
