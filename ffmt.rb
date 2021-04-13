88953cb6398c73f2c4baa85105406b4e7b93823c
Ivanka Heins <shinnova@users.noreply.github.com>
2021-04-13 15:45:09 +0200

    Update to 0.9.9 release

--- ffmt.rb
+++ ffmt.rb
@@ -14,7 +14,7 @@ end
 class Ffmt < Formula
   desc "FFXIV Modding Tool is the Cross-platform Commandline interface alternative for TexTools"
   homepage "https://ffmt.pwd.cat/"
-  url "https://github.com/fosspill/FFXIV_Modding_Tool.git", :tag => "v0.9.8", :revision => "74580577ca3dc5063231b82965c6f7e6c4bff61f"
+  url "https://github.com/fosspill/FFXIV_Modding_Tool.git", :tag => "v0.9.9", :revision => "7631c15ac9a1090b6685d8a571e5e75f0490b320"
   license "GPL-3.0"
 
   depends_on Dotnet => :build
